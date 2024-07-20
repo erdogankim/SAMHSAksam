9ed555cf139feaad997f4a0e5167e6047878ba42
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-07-20 18:44:27 +0300

    news2

--- Gun7_MutliDimensionalArrayExample/Gun7_MutliDimensionalArrayExample/Program.cs
+++ Gun7_MutliDimensionalArrayExample/Gun7_MutliDimensionalArrayExample/Program.cs
@@ -65,41 +65,71 @@
 // saat yönünde bir tur çevrilmiş halini oluşturan ve ekranda gösteren programı
 // yazınız. 
 
-int[,] dizi = new int[2, 2];
-int[,] reverse = new int[2, 2];
-Random rnd = new Random();
+//int[,] dizi = new int[2, 2];
+//int[,] reverse = new int[2, 2];
+//Random rnd = new Random();
 
-for (int i = 0; i < 2; i++)
-{
-    for (int j = 0; j < 2; j++)
-    {
-        dizi[i, j] = rnd.Next(1, 100);
-        Console.Write(dizi[i,j]);
-        Console.Write(" ");
-    }
-    Console.WriteLine();
-}
+//for (int i = 0; i < 2; i++)
+//{
+//    for (int j = 0; j < 2; j++)
+//    {
+//        dizi[i, j] = rnd.Next(1, 100);
+//        Console.Write(dizi[i,j]);
+//        Console.Write(" ");
+//    }
+//    Console.WriteLine();
+//}
 
 //   0,0  ->   0,1
 //   0,1  ->   1,1
 //   1,1  ->   1,0
 //   1,0  ->   0,0
 
-for (int i = 0; i < 2; i++)
+//for (int i = 0; i < 2; i++)
+//{
+//    for (int j = 0; j < 2; j++)
+//    {
+//        reverse[i, j+1] = dizi[i, j];
+//    }
+//}
+
+
+//for (int i = 0; i < 2; i++)
+//{
+//    for (int j = 0; j < 2; j++)
+//    {
+//        Console.Write(reverse[i, j]);
+//        Console.Write(" ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+// dışarıdan girilen bir yazının, her üç harfine göre parçalayan
+// bunu bir dizi değişkende saklayan programı yazınız. 
+
+Console.Write("Yazıyı giriniz ...:");
+string yazi = Console.ReadLine();
+
+int n = yazi.Length % 3 == 0 ? yazi.Length / 3 : (yazi.Length / 3) + 1;
+
+string[] arr = new string[n];
+int sayac = 0;
+
+for (int i = 0; i < yazi.Length; i+=3)
 {
-    for (int j = 0; j < 2; j++)
+    if (yazi.Length - i >= 3)
     {
-        reverse[i, j+1] = dizi[i, j];
+        arr[sayac] = yazi.Substring(i, 3); 
     }
+    else
+    {
+        arr[sayac] = yazi.Substring(i);
+    }
+    sayac++;
 }
 
-
-for (int i = 0; i < 2; i++)
+for (int i = 0; i < arr.Length; i++)
 {
-    for (int j = 0; j < 2; j++)
-    {
-        Console.Write(reverse[i, j]);
-        Console.Write(" ");
-    }
-    Console.WriteLine();
+    Console.WriteLine(arr[i]);
 }
